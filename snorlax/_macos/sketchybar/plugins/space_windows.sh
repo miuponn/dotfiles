504537086626672a8c3768ee1c4c4d341b475c19
miuponn <kellygao@live.ca>
2026-06-08 20:04:16 -0400

    cleanup for refactor.

--- snorlax/_macos/sketchybar/plugins/space_windows.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-source "$CONFIG_DIR/colors.sh"
-
-if [ "$SENDER" = "mouse.entered" ]; then
-  sketchybar --animate $ANIM_CURVE $ANIM_DURATION --set "$NAME" popup.drawing=on
-elif [ "$SENDER" = "mouse.exited" ]; then
-  sketchybar --animate $ANIM_CURVE $ANIM_DURATION --set "$NAME" popup.drawing=off
-fi
--- snorlax/nvim/lua/grove/palette.lua
+++
@@ -1,8 +0,0 @@
-return {
-	base00 = "#13242a", base01 = "#1b2f34", base02 = "#263d42", base03 = "#5a7880",
-	base04 = "#8098a0", base05 = "#e4ebe4", base06 = "#eef4ec", base07 = "#f6faf4",
-	base08 = "#e878a8", base09 = "#e89878", base0A = "#e8c888", base0B = "#78b098",
-	base0C = "#58b0b8", base0D = "#5090b0", base0E = "#a890c0", base0F = "#78786a",
-	teal_dim = "#4a8c92", teal_bright = "#6ec5cd",
-	lime = "#91b846", lime_bright = "#9bc73b", periwinkle = "#7887ba",
-}
